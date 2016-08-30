c92d8e045d6b8570eda1e4c638100d35ae059f6b
WindArrow <wind.dong.dream@gmail.com>
2016-08-30 11:17:43 +0900

    주소록 불러오기
    기존의 주소록 API가 deprecate 됌(뭔진 모르지만)
    현재의 API는 Contacts를 import를 하면 사용할 수 있으며 새로 생긴 기능으로는 페이스북 주소록 같이 외부 앱의
    주소록도 컨트롤 할 수 있다는 것이다.

--- OneDay/SettingViewController.swift
+++ OneDay/SettingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Contacts
 
 class SettingViewController: UIViewController {
 
@@ -15,7 +16,18 @@ class SettingViewController: UIViewController {
     @IBAction func SyncContract(sender: UIButton) {
         // 동기화 버튼 클릭
         // 연락처 동기화
-        
+        let store = CNContactStore()
+        let fetchRequest = CNContactFetchRequest(keysToFetch: [CNContactPhoneNumbersKey])
+        do {
+            try store.enumerateContactsWithFetchRequest(fetchRequest, usingBlock: {contact, stop in
+                print(contact.phoneNumbers)
+            })
+        } catch let err {
+            print(err)
+            let alert = UIAlertController(title: "에러", message: "에러", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "주소록 접근 실패", style: .Default, handler: nil))
+            presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad() {
